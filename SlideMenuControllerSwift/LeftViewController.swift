4ee43ad47999c39cbd6abedac5d24d85e6b308cd
Attila Dan <dn.attila@icloud.com>
2016-06-06 12:54:22 +0200

    left side menu

--- SlideMenuControllerSwift/LeftViewController.swift
+++ SlideMenuControllerSwift/LeftViewController.swift
@@ -26,6 +26,7 @@ protocol LeftMenuProtocol : class {
 class LeftViewController : UIViewController, LeftMenuProtocol {
     
     @IBOutlet weak var tableView: UITableView!
+    
     var menus = ["Összes Hirdetés", "Személyes", "Üzenetek", "Számlázás", "Hirdetéseim", "Kedvenceim","Hirdetésfigyelő","Ügynökség","Meghívás"]
     var mainViewController: UIViewController!
     var swiftViewController: UIViewController!
@@ -44,6 +45,8 @@ class LeftViewController : UIViewController, LeftMenuProtocol {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.tableView.registerCellClass(MenuItemViewController.self)
+        self.tableView.registerCellNib(MenuItemView.self)
         self.tableView.separatorColor = UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1.0)
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -72,7 +75,6 @@ class LeftViewController : UIViewController, LeftMenuProtocol {
         let inviteVWController = storyboard.instantiateViewControllerWithIdentifier("InviteViewController") as! InviteViewController
         self.inviteVWController = UINavigationController(rootViewController: inviteVWController)
         
-        self.tableView.registerCellClass(BaseTableViewCell.self)
         
         self.imageHeaderView = ImageHeaderView.loadNib()
         self.view.addSubview(self.imageHeaderView)
@@ -117,7 +119,7 @@ extension LeftViewController : UITableViewDelegate {
         if let menu = LeftMenu(rawValue: indexPath.item) {
             switch menu {
             case .Main, .Swift, .Java, .Go, .NonMenu, .FavMenu, .Admonitor, .AgenciesMenu, .Invite:
-                return BaseTableViewCell.height()
+                return MenuItemView.height()
             }
         }
         return 0
@@ -131,16 +133,10 @@ extension LeftViewController : UITableViewDataSource {
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        if let menu = LeftMenu(rawValue: indexPath.item) {
-            switch menu {
-            case .Main, .Swift, .Java, .Go, .NonMenu, .FavMenu, .Admonitor, .AgenciesMenu, .Invite:
-                let cell = BaseTableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: BaseTableViewCell.identifier)
-                cell.setData(menus[indexPath.row])
-                return cell
-            }
-        }
-        return UITableViewCell()
+        let cell = self.tableView.dequeueReusableCellWithIdentifier(MenuItemView.identifier) as! MenuItemView
+        let data = MenuItemViewCellData(imageUrl_menu: "dummy", text_menu: menus[indexPath.row])
+        cell.setData(data)
+        return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
---
+++ SlideMenuControllerSwift/MenuItemView.swift
@@ -0,0 +1,42 @@
+//
+//  MenuItemViewController.swift
+//  Bonodom
+//
+//  Created by Attila Dán on 2016. 06. 06..
+//  Copyright © 2016. Itechline. All rights reserved.
+//
+
+import UIKit
+
+struct MenuItemViewCellData {
+    
+    init(imageUrl_menu: String, text_menu: String) {
+        self.imageUrl_menu = imageUrl_menu
+        self.text_menu = text_menu
+    }
+    var imageUrl_menu: String
+    var text_menu: String
+}
+
+class MenuItemView: BaseMenuItemViewController {
+
+    @IBOutlet weak var dataImage: UIImageView!
+    @IBOutlet weak var dataText: UILabel!
+    
+    override func awakeFromNib() {
+        self.dataText?.font = UIFont.boldSystemFontOfSize(16)
+        self.dataText?.textColor = UIColor(hex: "000000")
+    }
+    
+    override class func height() -> CGFloat {
+        return 50
+    }
+    
+    override func setData(data: Any?) {
+        if let data = data as? MenuItemViewCellData {
+            self.dataImage.setRandomDownloadImage(50, height: 50)
+            self.dataText.text = data.text_menu
+        }
+    }
+
+}
