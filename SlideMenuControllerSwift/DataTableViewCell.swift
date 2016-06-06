85b682eacb9911d321abe120a8d227d08e04b195
Attila Dan <dn.attila@icloud.com>
2016-06-06 09:50:03 +0200

    stuff

--- SlideMenuControllerSwift/DataTableViewCell.swift
+++ SlideMenuControllerSwift/DataTableViewCell.swift
@@ -1,11 +1,3 @@
-//
-//  DataTableViewCell.swift
-//  SlideMenuControllerSwift
-//
-//  Created by Yuji Hato on 11/8/15.
-//  Copyright © 2015 Yuji Hato. All rights reserved.
-//
-
 import UIKit
 
 struct DataTableViewCellData {
