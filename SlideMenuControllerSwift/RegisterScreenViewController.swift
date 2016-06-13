86e0aada3dc64e7531682fb7397f8a55a9a86c5c
danati <dn.attila@icloud.com>
2016-06-13 14:35:08 +0200

    first setting pages

--- SlideMenuControllerSwift/RegisterScreenViewController.swift
+++ SlideMenuControllerSwift/RegisterScreenViewController.swift
@@ -85,14 +85,9 @@ class RegisterScreenViewController: UIViewController {
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func ProfileTypeSelector(sender: UISegmentedControl) {
     }
-    */
+    
 
 }
